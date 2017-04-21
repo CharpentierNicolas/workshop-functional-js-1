3f0a67c7c93c734a904525a72656c84428e6e223
CharpentierNicolas <nicolas.charpentier@ynov.com>
2017-04-21 09:31:52 +0200

    Exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -3,8 +3,16 @@ const chalk = require('chalk');
 console.log(chalk.cyan('\nEXERCISE 1 LOADED\n'));
 
 const zip = (input) => {
+	return input.reduce(function (acc, arr, i) {
 
-  // Your future job begins here ...
+        while (acc.length < arr.length) {
+            acc.push([]);
+        }
+        for (var j = 0; j < arr.length; ++j) {
+            acc[j][i] = arr[j];
+        }
+        return acc;
+    }, []);
 }
 
 module.exports = {
